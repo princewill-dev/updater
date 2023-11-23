5b34a71555d14493da8e0f4bc4feedde2b2f7990
princewill-dev <ezekudep@gmail.com>
2023-11-22 19:26:18 -0800

    Delete unnecessary files and code

--- updater/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Test
-
-class TestAdmin(admin.ModelAdmin):
-    list_display = ('id','text', 'ip_address', 'date')
-
-admin.site.register(Test, TestAdmin)
